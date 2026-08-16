55b9341c5238a4eed745c060dc473cf8588b32d9
proto-gs <georgsmerdov@outlook.com>
2026-08-16 19:09:55 +1000

    Configure release signing and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     id("org.jetbrains.kotlin.android")
@@ -5,15 +7,28 @@ plugins {
 
 android {
     namespace = "com.proxy.gshttp"
-
     compileSdk = 35
+
+    signingConfigs {
+        create("release") {
+            val properties = Properties().apply {
+                val propertiesFile = rootProject.file("local.properties")
+                if (propertiesFile.exists()) {
+                    load(propertiesFile.inputStream())
+                }
+            }
+
+            storeFile = file("proto-gs-key-for-gshttp.jks")
+            storePassword = properties.getProperty("RELEASE_STORE_PASSWORD")
+            keyAlias = properties.getProperty("RELEASE_KEY_ALIAS")
+            keyPassword = properties.getProperty("RELEASE_KEY_PASSWORD")
+        }
+    }
 
     defaultConfig {
         applicationId = "com.proxy.gshttp"
         minSdk = 24
-
         targetSdk = 35
-
         versionCode = 6
         versionName = "1.0.6"
 
@@ -23,8 +38,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
 
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -32,14 +46,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
+
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
@@ -48,7 +66,6 @@ android {
             jvmTarget = "11"
         }
     }
-
 
     applicationVariants.all {
         val variant = this
@@ -64,10 +81,14 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-graphics:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.material3:material3:1.1.2")
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    implementation("androidx.compose.material:material-icons-extended")
 
     libs.okhttp?.let { implementation(it) }
 
@@ -75,8 +96,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-    implementation("androidx.compose.material:material-icons-extended")
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
